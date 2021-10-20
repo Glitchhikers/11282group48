483ca9c2d39604e2e138e11594e0f10a921dacec
yonasbahre <yonasbahre7@gmail.com>
2021-10-20 01:10:54 -0400

    Revert "Revert "Updated frontend template w/ ability to call backend, also added Spring backend""
    This reverts commit fa7229fd

---
+++ CarpoolConnectBackend/src/main/java/com/example/CarpoolConnectBackend/CarpoolConnectBackendApplication.java
@@ -0,0 +1,15 @@
+package com.example.CarpoolConnectBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpoolConnectBackendApplication {
+
+	// Main function for backend. We don't need to change anything in this file.
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarpoolConnectBackendApplication.class, args);
+	}
+
+}
